3f664cbd24553f68d409e6f1a176c6efb418b047
ZenBarrier <anthony.j.barrera@gmail.com>
2016-10-26 14:20:47 -0400

    Show list of users

--- app/src/main/java/com/zenbarrier/instagramclone/UserListActivity.java
+++ app/src/main/java/com/zenbarrier/instagramclone/UserListActivity.java
@@ -2,12 +2,43 @@ package com.zenbarrier.instagramclone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserListActivity extends AppCompatActivity {
+
+    ArrayList<String> userList = new ArrayList<>();
+    ArrayAdapter<String> adapter;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_list);
+
+        listView = (ListView) findViewById(R.id.listUsers);
+
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.whereNotEqualTo("username", ParseUser.getCurrentUser().getUsername());
+        query.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> objects, ParseException e) {
+                if(e==null){
+                    for(ParseUser user:objects){
+                        userList.add(user.getUsername());
+                    }
+                    adapter = new ArrayAdapter<>(UserListActivity.this, android.R.layout.simple_list_item_1, userList);
+                    listView.setAdapter(adapter);
+                }
+            }
+        });
     }
 }
