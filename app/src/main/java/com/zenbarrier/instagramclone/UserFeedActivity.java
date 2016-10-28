1b9ebb440bdd78d7601fae3dc01299a219020b7d
ZenBarrier <anthony.j.barrera@gmail.com>
2016-10-27 20:08:30 -0400

    display images within linearLayout

--- app/src/main/java/com/zenbarrier/instagramclone/UserFeedActivity.java
+++ app/src/main/java/com/zenbarrier/instagramclone/UserFeedActivity.java
@@ -1,21 +1,31 @@
 package com.zenbarrier.instagramclone;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.parse.FindCallback;
+import com.parse.GetDataCallback;
+import com.parse.Parse;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
 import java.util.List;
 
 public class UserFeedActivity extends AppCompatActivity {
+
+    LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +44,8 @@ public class UserFeedActivity extends AppCompatActivity {
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+
 
         Intent intent = getIntent();
         String userId = intent.getStringExtra("userId");
@@ -43,11 +55,31 @@ public class UserFeedActivity extends AppCompatActivity {
 
         ParseQuery<ParseObject> query = ParseQuery.getQuery("Images");
         query.whereEqualTo("userId", userId);
+        query.orderByAscending("createdAt");
 
         query.findInBackground(new FindCallback<ParseObject>() {
             @Override
             public void done(List<ParseObject> objects, ParseException e) {
-
+                if(e == null){
+                    for(ParseObject object:objects){
+                        ParseFile file = (ParseFile) object.get("image");
+                        file.getDataInBackground(new GetDataCallback() {
+                            @Override
+                            public void done(byte[] data, ParseException e) {
+                                if(e==null){
+                                    Bitmap image = BitmapFactory.decodeByteArray(data, 0, data.length);
+                                    ImageView imageView = new ImageView(getApplicationContext());
+                                    imageView.setImageBitmap(image);
+                                    imageView.setLayoutParams(new ViewGroup.LayoutParams(
+                                            ViewGroup.LayoutParams.MATCH_PARENT,
+                                            ViewGroup.LayoutParams.WRAP_CONTENT
+                                    ));
+                                    linearLayout.addView(imageView);
+                                }
+                            }
+                        });
+                    }
+                }
             }
         });
     }
